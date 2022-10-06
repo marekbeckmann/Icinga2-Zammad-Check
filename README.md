2089419b9df89ef9c56e85c68b1ae1a6027d6307
marekbeckmann <marek.beckmann@bektroid-studios.de>
2022-10-06 15:50:11 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# Icinga2-Zammad-Check
+# Zammad Icinga2 Checks
+
+## 1. Functionality
+
+This plugin gives you the ability, to display the Zammad Monitoring Status within Icinga2, using the Zammad API. 
+
+## 2. Script Usage:
+
+Clone this repository, and copy the `check_zammad.py` script to your Icinga2 Plugin Directory.
+You may need to grant the script permission and assign the correct owner.
+
+```bash
+git clone https://github.com/marekbeckmann/Icinga2-Zammad-Check zammad-icinga2-checks
+cd zammad-icinga2-checks
+cp check_zammad.py /usr/lib/nagios/plugins/
+chmod +x /usr/lib/nagios/plugins/check_zammad.py
+chown nagios /usr/lib/nagios/plugins/check_zammad.py
+```
+
+To execute the script, run the following command: 
+```bash
+python3 check_zammad.py <args>
+```
+
+You can run the script with the following arguments: 
+| argument                | Required | description                |
+| ----------------------- | -------- | -------------------------- |
+| `-h`                    | ✅        | Show help message and exit |
+| `--server`   `<url>`    | ✅        | URL of Zammad instance     |
+| `--token`     `<token>` | ✅        | Token                      |
+
+You can get the token from here: https://zammad.example.org/#system/monitoring
+
+**Important:** The Server URL mustn't end with a slash. E.g: https://zammad.example.org
+
+## 2. Icinga Config (example):
+
+`services.conf`
+```
+apply Service "zammadcheck" {
+  import "generic-service"
+  check_command = "check-zammad"
+  assign where host.name == "kitz-ithd"
+}
+```
+
+`commands.conf`
+```
+object CheckCommand "check-zammad" {
+  import "plugin-check-command"
+
+  command = [PluginDir + "/check_zammad"]
+
+    arguments = {
+      "--server" = "SERVER_URL"
+      "--token" =  "TOKEN"
+    }
+}
+```
+
+---
+
+## Sample Output: 
+```bash
+$ python3 check_zammad.py --server example.zammad.org --token 12345
+```
+```ini
+Zammad Health Message:
+Issues: No issues
+Actions: No actions
+```
---
+++ check_zammad.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+import requests
+import json
+import sys
+import argparse
+####
+#
+#  Complete Documentation: https://github.com/marekbeckmann/Icinga2-Zammad-Check
+#
+####
+
+# STATES
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+# Icinga Message
+message = {
+    'status': OK,
+    'summary': 'Zammad Health Message: ',
+}
+
+
+def check(server, token):
+
+    try:
+        zammad_data = requests.get(
+            server + "/api/v1/monitoring/health_check?token=" + token).text
+
+        zammad_data = json.loads(zammad_data)
+        status = UNKNOWN
+    except:
+        status = CRITICAL
+        message['summary'] = 'Zammad Health information retrieval failed!'
+        return status
+
+    health = zammad_data['healthy']
+    issues = zammad_data['issues']
+    actions = zammad_data['actions']
+    if not issues:
+        issues = "No issues"
+    if not actions:
+        actions = "No actions"
+
+    if health:
+        status = OK
+        message['summary'] += '\nIssues: ' + issues
+        message['summary'] += '\nActions: ' + actions
+
+    else:
+        status = WARNING
+        message['summary'] += '\nIssues: ' + issues
+        message['summary'] += '\nActions: ' + actions
+    return status
+
+
+def args():
+    parser = argparse.ArgumentParser(
+        prog=sys.argv[0],
+        description='Icinga/Nagios Check Script for Zammad',
+        epilog="Thanks for using my Plugin. \nDocumentation: https://github.com/marekbeckmann/Icinga2-Zammad-Check"
+    )
+
+    parser.add_argument(
+        "--token",
+        help="Specify the Zammad Monitoring Token.",
+        required=True,
+        action='store',
+        type=str
+    )
+
+    parser.add_argument(
+        "--server",
+        required=True,
+        action='store',
+        type=str
+    )
+
+    return parser
+
+
+args = args().parse_args()
+token = args.token
+server = args.server
+
+message['status'] += check(server, token)
+print("{summary}".format(
+    summary=message.get('summary'),
+))
+raise SystemExit(message['status'])
